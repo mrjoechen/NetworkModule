24298f82ca78c60309243acf92a869d0dab57a11
mrjoechen <mrjoechen@qq.com>
2019-03-18 10:27:01 +0800

    init

---
+++ webserver/src/com/imooc/http/HelloServlet.java
@@ -0,0 +1,58 @@
+package com.imooc.http;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+
+/**
+ * Servlet implementation class HelloServlet
+ */
+@WebServlet("/HelloServlet")
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath()+"\n");
+		this.doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String username=request.getParameter("username");
+		String userage=request.getParameter("userage");
+		
+		Gson gson=new Gson();
+		
+		Person person=new Person();
+		person.setAge(Integer.parseInt(userage));
+		person.setName(username);
+		response.setContentType("application/json;charset=UTF-8");
+	    response.setCharacterEncoding("utf-8");  
+		response.getWriter().write(gson.toJson(person));
+
+	
+		
+		
+	}
+
+}
---
+++ webserver/src/com/imooc/http/UploadServlet.java
@@ -0,0 +1,56 @@
+package com.imooc.http;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+
+/**
+ * Servlet implementation class UploadServlet
+ */
+@MultipartConfig
+@WebServlet("/UploadServlet")
+public class UploadServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public UploadServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+		String name=request.getParameter("name");
+		Part part=request.getPart("filename");
+		PrintWriter out=response.getWriter();
+		out.print("name="+name+"\n");
+		out.print("ContentType="+part.getContentType()+"\n");
+		out.print("size="+part.getSize()+"\n");
+		part.write("/Users/nate/girl_server.jpg");
+		
+		
+		
+	}
+
+}
